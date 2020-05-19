741389f6108c21adcf698c4e7ac379cbbf8ae3bd
Elie Hymowitz <ehymowitz@gmail.com>
2020-05-19 14:09:53 -0400

    Added skeleton of controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+
   end
 end
