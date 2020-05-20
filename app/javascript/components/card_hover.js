0dd6762a39db01144f45c42ca164f429d59a82d5
Stephanie Diep <stephaniediep@outlook.com>
2020-05-20 14:10:41 -0400

    Added img being bigger when hover

--- app/javascript/components/card_hover.js
+++ app/javascript/components/card_hover.js
@@ -1,15 +1,15 @@
 // Code doesn't work for now, need some work
-// const cardHover = () => {
-//   const card_img = document.querySelector('#homepage-card img');
-//   console.log(card_img);
+const cardHover = () => {
+  const card_img = document.querySelector('#homepage-card img');
+  console.log(card_img);
 
-//   card_img.addEventListener('mouseover', (event) => {
-//     console.log(event)
-//     const card_info = document.querySelector('.card-small-info');
-//     console.log(card_info);
-//     event.currentTarget.classList.remove(".card-small-info");
-//     event.currentTarget.classList.add(".card-active");
-//   })
-// }
+  card_img.addEventListener('mouseover', (event) => {
+    console.log(event)
+    const card_info = document.querySelector('.card-small-info');
+    console.log(card_info);
+    event.currentTarget.classList.remove(".card-small-info");
+    event.currentTarget.classList.add(".card-active");
+  })
+}
 
-// export { cardHover };
+export { cardHover };
