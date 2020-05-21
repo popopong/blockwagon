2457773d6e0ee30884f46e303d3c776dd4f03b15
Elie Hymowitz <ehymowitz@gmail.com>
2020-05-21 11:55:18 -0400

    Lunch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,13 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+    @user.update(user_params)
   end
 
   private
 
-
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
 
 end
