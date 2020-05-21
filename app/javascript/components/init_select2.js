390d8ae85fd9f02beddd858be2da0127de6cd53b
Stephanie BeauzileD <stephanie.beauzile.d@gmail.com>
2020-05-21 16:58:31 -0400

    completed the form with genre selection

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('#video_cassette_genre_ids').select2({ width: '100%' });
+  $('#video_cassette_genre_ids').select2({ width: '100%', placeholder: "Select a genre" });
 };
 
 
