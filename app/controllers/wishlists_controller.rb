5da53b2868503b77889f8a0124635ba070ea5f03
Stephanie Diep <stephaniediep@outlook.com>
2020-05-21 14:39:56 -0400

    Added wishlist pundit

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -2,7 +2,7 @@ class WishlistsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @wishlists = Wishlist.all
+    @wishlists = policy_scope(Wishlist.all)
   end
 
   # Form to add a VHS to wishlist will be on the Videos Show page
@@ -10,6 +10,9 @@ class WishlistsController < ApplicationController
     @wishlist = Wishlist.new
     @wishlist.user = current_user
     @wishlist.video_cassette_id = params[:video_cassette_id]
+
+    authorize @wishlist
+
     @wishlist.save
     redirect_to video_cassette_path(params[:video_cassette_id])
   end
@@ -17,6 +20,8 @@ class WishlistsController < ApplicationController
   def destroy
     @wishlist = Wishlist.find(params[:id])
     redirect_to video_cassette_path(@wishlist.video_cassette)
+
+    authorize @wishlist
     @wishlist.destroy
   end
 end
