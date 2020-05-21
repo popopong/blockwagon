b433d06bda73dd51fdb8d36043ca86a7dc7994ea
Elie Hymowitz <ehymowitz@gmail.com>
2020-05-21 17:32:46 -0400

    Looking good there, mr fancy pants

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,9 +1,19 @@
 const navbarTransparent = () => {
-}
+  let target = document.querySelector(".navbar")
 
-window.addEventListener('scroll', event => {
-  let height = window.scrollY;
-  console.log(height);
-})
+
+  window.addEventListener('scroll', event => {
+    let height = (window.scrollY/window.innerHeight)*100;
+    if (height > 10 ){
+      target.style.backgroundColor = 'rgba(255,255,255,0.7)';
+      target.firstElementChild.innerText = "BL"
+      target.style.textShadow = "1px 0px 10px lightgrey"
+    } else {
+      target.style.backgroundColor = 'rgba(255,255,255,1)';
+      target.firstElementChild.innerText = "Blockwagon"
+      target.style.textShadow = ""
+    }
+  })
+}
 
 export { navbarTransparent }
