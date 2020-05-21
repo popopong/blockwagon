4dafdf5eba87d3ee735f58bbbd932d90f0d7a6d4
Elie Hymowitz <ehymowitz@gmail.com>
2020-05-21 11:44:29 -0400

    main page content done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  end
+
+  private
+
+
+
 end
