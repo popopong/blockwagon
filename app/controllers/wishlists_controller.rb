1e958c5a37c35501ad7b7f48180c4f27850b313a
Elie Hymowitz <ehymowitz@gmail.com>
2020-05-21 15:13:02 -0400

    Info there, needs formatting

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -2,7 +2,10 @@ class WishlistsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @wishlists = Wishlist.all
+    @wishlists = Wishlist.where(user_id: current_user.id)
+    @video_ids = @wishlists.map{ |video_id|
+      VideoCassette.find(video_id.video_cassette_id)
+    }
   end
 
   # Form to add a VHS to wishlist will be on the Videos Show page
