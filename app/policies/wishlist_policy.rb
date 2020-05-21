99f31f49a4f910a2da97d63e78fb1ee150a435f4
Stephanie Diep <stephaniediep@outlook.com>
2020-05-21 14:30:58 -0400

    Added scope

--- app/policies/wishlist_policy.rb
+++ app/policies/wishlist_policy.rb
@@ -1,11 +1,7 @@
 class WishlistPolicy < ApplicationPolicy
-  def index?
-    Wishlist.user == user
-  end
-
   class Scope < Scope
     def resolve
-      scope.all
+      scope.where(user: user)
     end
   end
 end
