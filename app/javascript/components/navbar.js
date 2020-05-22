ba3a74b47eabe12ea36b589caa0bd6140e388456
Poyan Ng <poyanng.npy@gmail.com>
2020-05-22 15:12:33 -0400

    basic color added

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -5,11 +5,11 @@ const navbarTransparent = () => {
   window.addEventListener('scroll', event => {
     let height = (window.scrollY/window.innerHeight)*100;
     if (height > 10 ){
-      target.style.backgroundColor = 'rgba(255,255,255,0.7)';
+      target.style.backgroundColor = 'rgba(26, 29, 41, 0.7)';
       target.firstElementChild.innerText = "BL"
       target.style.textShadow = "1px 0px 10px lightgrey"
     } else {
-      target.style.backgroundColor = 'rgba(255,255,255,1)';
+      target.style.backgroundColor = 'rgba(26, 29, 41, 1)';
       target.firstElementChild.innerText = "Blockwagon"
       target.style.textShadow = ""
     }
